cb0c163c5e58b6f2534ed788a328393ee65dc80b
Nithish Velu <n.kumaraguru540@mybvc.ca>
2025-07-07 21:11:21 -0600

    New Animations and SCripts
    Animations - Aim idle and rifle aim idle
    Scripts - added new folder AimState Manager and inside AimBaseState, AimState, HipFireState

--- Assets/Scripts/AimStateManager/AimStateManager.cs
+++ Assets/Scripts/AimStateManager/AimStateManager.cs
@@ -2,17 +2,27 @@ using UnityEngine;
 using Unity.Cinemachine;
 public class AimStateManager : MonoBehaviour
 {
+    AimBaseState currentState;
+    public HipFireState Hip = new HipFireState();
+    public AimState Aim = new AimState();
+
     [SerializeField] private Transform cameraTransform;
-    
+
+    [HideInInspector] public Animator anim;
 
     private void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
 
+        anim = GetComponentInChildren<Animator>();
+        SwitchState(Hip);
+
     }
     void Update()
     {
         RotatePlayerToCamera();
+
+        currentState.UpdateState(this);
     }
 
     private void RotatePlayerToCamera()
@@ -23,5 +33,11 @@ public class AimStateManager : MonoBehaviour
 
         if (viewDirection != Vector3.zero)
             transform.forward = viewDirection;  
+    }
+
+    public void SwitchState(AimBaseState state)
+    {
+        currentState = state;
+        currentState.EnterState(this);
     }
 }
