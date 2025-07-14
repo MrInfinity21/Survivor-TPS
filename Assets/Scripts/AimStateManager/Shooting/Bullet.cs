660318c66c4222894037ee2bf174c82d2b6677b8
Nithish Velu <n.kumaraguru540@mybvc.ca>
2025-07-13 23:47:16 -0600

    Destory Enemy
    Added CrossHair and Destroy enemy when the bullet hits the cylinder(enemy)

--- Assets/Scripts/AimStateManager/Shooting/Bullet.cs
+++ Assets/Scripts/AimStateManager/Shooting/Bullet.cs
@@ -3,21 +3,25 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    [SerializeField] float _timeToDestroy;
+    [SerializeField] float _timeToDestroy = 5f;
     float _timer;
-    void Start()
-    {
-            
-    }
+
 
     void Update()
     {
         _timer += Time.deltaTime;
-        if (_timer >= _timeToDestroy) Destroy(this.gameObject); 
+        if (_timer >= _timeToDestroy)
+        {
+            Destroy(this.gameObject);
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(collision.gameObject);
+        }
         Destroy(this.gameObject);
     }
 }
