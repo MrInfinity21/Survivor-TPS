3a60c3ff8b55c3bc5480d6a326b64eb4b1b10ebc
Nithish Velu <n.kumaraguru540@mybvc.ca>
2025-07-17 12:51:36 -0600

    Just making or adding a change
    Input System Script (Done)

---
+++ Assets/Scripts/Player_Movements/PlayerInputController.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+
+public class PlayerInputController : MonoBehaviour
+{
+    InputSystem_Actions _actions;
+
+    private void OnEnable()
+    {
+        _actions = new InputSystem_Actions();
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
